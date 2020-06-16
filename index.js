ff45576d6c05aee818c604c0fba7aee0592a8fc0
Joshua Stoll <jds21171@uga.edu>
2020-06-16 14:56:31 -0400

    heroku deployment now works

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ var db = require("./models");
 var session = require("express-session");
 const isAuthenticated = require('./config/middleware/isAuthenticated');
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 5000;
+// app.listen(port, host, function() {
+//   console.log("Server started.......");
+// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,16 +26,11 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-// API key - pk_c31bdade5c47425fafb25d4831e9861e //
-// NYT API - QzKMS3OtIrTljBwjuddSWl23yFk5P70N
-
 // create call_api function //
 function call_api(finishedAPI, ticker) {
   request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_c31bdade5c47425fafb25d4831e9861e', { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
     if (res.statusCode === 200) {
-      //console.log(body);
       finishedAPI(body);
     };
   });
@@ -38,35 +39,31 @@ function call_api(finishedAPI, ticker) {
 //multiple API calls
 const fetchApiInfo = async (url) => {
   console.log(`Fetching ${url}`)
-  const stockInfo = await axios(url) // API call 
+  const stockInfo = await axios(url)
   return stockInfo.data;
 }
 // Iterates through array and return stock info
 const fetchUserInfo = async (tickers) => {
   const requests = tickers.map((ticker) => {
     const url = `https://cloud.iexapis.com/stable/stock/` + ticker + `/quote?token=pk_c31bdade5c47425fafb25d4831e9861e`
-    return fetchApiInfo(url) // Async function that fetches the user info.
+    return fetchApiInfo(url)
       .then((res) => {
         return res
       })
   })
-  return Promise.all(requests) // Waiting for all the requests to get resolved.
+  return Promise.all(requests)
 };
-//invoke fetch user info
-// fetchUserInfo([‘fb’, ‘tsla’, ‘aapl’])
-// .then(a => console.log(JSON.stringify(a)))
 
-
-
+// Iterates through array to return news info
 const fetchNewsInfo = async (tickers) => {
   const requests = tickers.map((ticker) => {
     const url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + ticker + '&facet_fields=source&facet=true&begin_date=20200601&end_date=20200625&api-key=QzKMS3OtIrTljBwjuddSWl23yFk5P70N'
-    return fetchApiInfo(url) // Async function that fetches the user info.
+    return fetchApiInfo(url)
       .then((res) => {
         return res
       })
   })
-  return Promise.all(requests) // Waiting for all the requests to get resolved.
+  return Promise.all(requests)
 };
 
 // Setting the handlebars middleware //
@@ -82,25 +79,6 @@ app.get('/members', isAuthenticated, function (req, res) {
   }, "fb");
 });
 
-// // Here we've add our isAuthenticated middleware to this route.
-// // If a user who is not logged in tries to access this route they will be redirected to the signup page
-// app.get("/members", isAuthenticated, function (req, res) {
-//   res.redirect("/");
-// });
-
-// In case the above "GET" doesn't work properly, you can use the one below instead.
-
-// app.get("", function (req, res) {
-//   if (req.user) {
-//     call_api(function (doneAPI) {
-//       res.render('home', {
-//         stock: doneAPI
-//       });
-//     }, "fb");
-//     } else {
-//       res.redirect("/login");
-//     }
-//   });
 app.get("/", function (req, res) {
   if (req.user) {
     res.redirect("/members")
@@ -110,7 +88,7 @@ app.get("/", function (req, res) {
 })
 
 
-// Create about page route //
+// Create about page route 
 app.get('/about', async function (req, res) {
   if (!req.user) {
     res.redirect("/login");
@@ -123,6 +101,7 @@ app.get('/about', async function (req, res) {
   }
 });
 
+// Creating portfolio page route
 app.get('/portfolio', async function (req, res) {
   if (!req.user) {
     res.redirect("/login");
@@ -135,6 +114,7 @@ app.get('/portfolio', async function (req, res) {
   }
 });
 
+//Creating news page route
 app.get('/news', async function (req, res) {
   if (!req.user) {
     res.redirect("/login");
@@ -158,17 +138,16 @@ app.post('/', function (req, res) {
 
 });
 
-// Requring all the routes in our code //
+// Requring additional routes in our code //
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.log(
-      "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// db.sequelize.sync().then(function () {
+//   app.listen(port, host, function() {
+//     console.log("Server started.......");
+//   });
+// });
+app.listen(port, host, function() {
+  console.log("Server started.......");
 });
